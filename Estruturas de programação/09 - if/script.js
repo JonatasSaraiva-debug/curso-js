820ae97a91000f25517a3a3a401feabbe8b7f982
JonatasSaraiva-debug <jonatassaraiva90@gmail.com>
2026-03-11 23:51:05 -0300

    mais atividades

---
+++ Estruturas de programação/09 - if/script.js
@@ -0,0 +1,21 @@
+let idade = 19
+
+if (idade == 19) {
+    console.log('Você tem 19 anos')
+}
+
+if (idade > 25) {
+    console.log('Você tem mais de 25 anos')
+}
+
+let nome = "Matheus"
+
+if (nome == "Matheus" && idade > 10) {
+    console.log("Liberado!")
+}
+
+let passaporte = true
+
+if (nome == "Matheus" && idade > 30 || passaporte == true) {
+    console.log("Liberado 2!")
+}
---
+++ Estruturas de programação/10-else/script.js
@@ -0,0 +1,6 @@
+let velocidade = 100
+if (velocidade < 80) {
+    console.log("não foi multado")
+} else {
+    console.log("foi multado!")
+}

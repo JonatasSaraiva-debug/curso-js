a569799be1df32f86d5bab28663af4c135c3ed47
JonatasSaraiva-debug <jonatassaraiva90@gmail.com>
2026-03-16 02:38:31 -0300

    conclusão
    conclusão de Estruturas de Programação

---
+++ Estruturas de programação/16 - break/script.js
@@ -0,0 +1,17 @@
+let nome = "Jonatas";
+
+
+for(let i = 0; i < 10; i = i + 1) {
+
+
+    if(i == 3) {
+        nome = "Maria";
+    }
+
+    if(i == 5 && nome == "Maria") {
+        console.log("O nome é Maria, pode parar")
+        break;
+    }
+
+    console.log(i);
+}
---
+++ Estruturas de programação/19 - switch/script.js
@@ -0,0 +1,19 @@
+let nome = "Matheus";
+
+switch (nome) {
+    case "Matheus":
+        console.log("o nome é Matheus");
+        break;
+    case "João":
+        console.log("o nome é João");
+        break;
+    default:
+        console.log("o nome não foi encontrado");
+        break;
+}
+
+// if (nome === "Matheus") {
+//    console.log("o nome é Matheus");
+//   else {
+//        console.log("o nome não foi encontrado");
+//}

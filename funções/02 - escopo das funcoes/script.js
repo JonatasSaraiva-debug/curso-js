4b98aa4318096b5317e0ddcc4ebc55210b6eaa1e
JonatasSaraiva-debug <jonatassaraiva90@gmail.com>
2026-03-30 04:59:12 -0300

    escopo das funcoes

---
+++ funções/02 - escopo das funcoes/script.js
@@ -0,0 +1,19 @@
+let y = 10;
+
+function imprimir() {
+    let y = 150;
+
+    console.log(y);
+}
+imprimir()
+console.log(y)
+
+let x = 11;
+
+if (x > 5)  {
+ let x = 20;
+x++;
+console.log(x);
+}
+
+console.log(x);

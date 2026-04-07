7cba5d34015ca17c9c1de096bda71e82ed173752
JonatasSaraiva-debug <jonatassaraiva90@gmail.com>
2026-04-07 05:14:21 -0300

    curso js

---
+++ funções/04 - arrow function/script.js
@@ -0,0 +1,11 @@
+let consoleTest = () => {
+    console.log("olá mundo!")
+};
+
+consoleTest();
+
+let soma = (a,b) => {
+    return a + b;
+};
+
+console.log(soma(5,5));
---
+++ funções/06 - argumentos opcionais/script.js
@@ -0,0 +1,10 @@
+function soma(a,b) {
+    if(a === undefined || b === undefined) {
+        console.log("essa funcao precisa dos dois argumentos");
+} else {
+    return a + b;
+}
+
+}
+console.log(soma(2,5));
+console.log(soma(1));

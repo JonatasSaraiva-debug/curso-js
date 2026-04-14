5b35daa836d7bcf5356cec77aa94ed2e70e91884
JonatasSaraiva-debug <jonatassaraiva90@gmail.com>
2026-04-14 01:40:43 -0300

    feat: practice javaScript fundamentals  (loops, functions, operators)

---
+++ funções-exercicios/07-js-string-length-check/script.js
@@ -0,0 +1,10 @@
+function string(text) {
+        if(text.length > 10) {
+                console.log("text too long")
+        } else {
+                console.log("text within limit")
+        }
+}
+
+string("java script ");
+string("jo");
---
+++ funções-exercicios/08-power-function/script.js
@@ -0,0 +1,4 @@
+function calculation(base,power) {
+        return base ** power;
+}
+console.log(calculation(3,2));

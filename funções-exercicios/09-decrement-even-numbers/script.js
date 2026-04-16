80840df2ef5092a05c67445eaaedc85e2012bb25
JonatasSaraiva-debug <jonatassaraiva90@gmail.com>
2026-04-16 01:34:55 -0300

    feat: print even numbers using decrement loop

---
+++ funções-exercicios/09-decrement-even-numbers/script.js
@@ -0,0 +1,10 @@
+function decrement(number) {
+        for(number > 0; number --;) {
+                if(number % 2 == 0){
+                        console.log(`number ${number} is even`);
+                }
+        }
+}
+
+decrement(30);
+decrement(10);
